ac62e014a2e38a678907d2041592fa9d7331a988
mabassoli <martinbassoli02@gmail.com>
2022-01-12 00:26:43 -0300

    first commit

---
+++ clase1Bassoli/main.js
@@ -0,0 +1,9 @@
+// FICHAJE DE OBREROS
+
+let user = prompt("Introduzca su nombre de usuario: ");
+
+alert("Nombre introducido: " + user);
+
+const DNI = Number(prompt("Introduzca su DNI: "));
+
+console.log(("Usuario: ") + user + (" DNI: ") + Number(DNI));
